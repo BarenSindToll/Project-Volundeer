48bee4448f637658faabb329cef3e5870c8f4b7f
AnaVit <anamariavit29@gmail.com>
2019-02-04 16:53:38 +0200

    js

--- design/js/index.js
+++ design/js/index.js
@@ -18,15 +18,3 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
-
-
-function expand() {
-  $(".search").toggleClass("close");
-  $(".input").toggleClass("square");
-  if ($('.search').hasClass('close')) {
-    $('input').focus();
-  } else {
-    $('input').blur();
-  }
-}
-$('button').on('click', expand);
